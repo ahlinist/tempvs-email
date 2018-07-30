c631b56ac4826e18f0e50580e40d4a7173191224
skajushka <anton.hlinisty@gmail.com>
2018-07-31 00:04:02 +0300

    #3. Test sendgrid email delivery

--- src/main/java/club/tempvs/email/api/Api.java
+++ src/main/java/club/tempvs/email/api/Api.java
@@ -5,10 +5,12 @@ import club.tempvs.email.auth.AuthenticationException;
 import club.tempvs.email.EmailService;
 import club.tempvs.email.json.Payload;
 import club.tempvs.email.json.PayloadMalformedException;
+import com.sendgrid.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 @Path("api")
 public class Api {
@@ -30,7 +32,28 @@ public class Api {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response sendEmail(Payload payload) {
         try {
-            emailService.doSend(payload, token);
+
+            Email from = new Email("test@tempvs.club");
+            String subject = "Hello World from the SendGrid Java Library!";
+            Email to = new Email("anton.hlinisty@gmail.com");
+            Content content = new Content("text/plain", "Hello, Email!");
+            Mail mail = new Mail(from, subject, to, content);
+
+            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+            Request request = new Request();
+            try {
+                request.setMethod(Method.POST);
+                request.setEndpoint("mail/send");
+                request.setBody(mail.build());
+                com.sendgrid.Response response = sg.api(request);
+                System.out.println(response.getStatusCode());
+                System.out.println(response.getBody());
+                System.out.println(response.getHeaders());
+            } catch (IOException ex) {
+                throw ex;
+            }
+
+            //emailService.doSend(payload, token);
             return Response.ok().build();
         } catch (AuthenticationException e) {
             return Response.status(401).build();
