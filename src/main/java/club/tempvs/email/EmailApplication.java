eec625a06d025820ab838299a5f9f0cc4c28093a
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-01-12 23:24:29 +0300

    Enable eureka discovery

--- src/main/java/club/tempvs/email/EmailApplication.java
+++ src/main/java/club/tempvs/email/EmailApplication.java
@@ -3,9 +3,11 @@ package club.tempvs.email;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class EmailApplication {
 
 	public static void main(String[] args) {
